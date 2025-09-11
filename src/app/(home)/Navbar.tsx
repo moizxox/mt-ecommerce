86c0cbbc54c706251ecb5060458fe844d94cf90a
moizxox <mianmoiz899@gmail.com>
2025-09-11 16:37:02 +0500

    Mobile Navbar added

--- src/app/(home)/Navbar.tsx
+++ src/app/(home)/Navbar.tsx
@@ -5,6 +5,9 @@ import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { usePathname } from "next/navigation";
+import NavbarSidebar from "./NavbarSidebar";
+import { useState } from "react";
+import { MenuIcon } from "lucide-react";
 const poppins = Poppins({ subsets: ["latin"], weight: ["700"] });
 
 interface NavbarItemProps {
@@ -53,12 +56,15 @@ const navbarItems = [
 
 export const Navbar = () => {
   const pathname = usePathname();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   return (
     <nav className="h-20 flex border-b justify-between font-medium bg-white">
       <Link href="/" className="flex items-center pl-6">
-        <span className={cn("text-5xl font-semibold", poppins.className)}>MT Ecommerce</span>
+        <span className={cn("text-5xl font-semibold", poppins.className)}>MT </span>
       </Link>
+
+      <NavbarSidebar items={navbarItems} open={isSidebarOpen} onOpenChange={setIsSidebarOpen} />
 
       <div className="items-center gap-4 hidden lg:flex">
         {navbarItems.map((item) => {
@@ -72,7 +78,6 @@ export const Navbar = () => {
       <div className="hidden lg:flex">
         <Link href="/sign-in">
           <Button
-            asChild
             variant="secondary"
             className="bg-transparent border-l border-r-0 border-t-0 border-b-0 rounded-none h-full text-blacj hover:bg-pink-400 transition-all px-12"
           >
@@ -80,13 +85,21 @@ export const Navbar = () => {
           </Button>
         </Link>
         <Link href="/sign-up">
-          <Button
-            asChild
-            className="bg-black border-l border-r-0 border-t-0 border-b-0 rounded-none h-full text-white hover:bg-pink-400 hover:text-black transition-all px-12"
-          >
+          <Button className="bg-black border-l border-r-0 border-t-0 border-b-0 rounded-none h-full text-white hover:bg-pink-400 hover:text-black transition-all px-12">
             Start Selling
           </Button>
         </Link>
+      </div>
+      <div className="flex lg:hidden h-full items-center">
+        <Button
+          variant="ghost"
+          className="border-transparent size-12 bg-white"
+          onClick={() => {
+            setIsSidebarOpen(true);
+          }}
+        >
+          <MenuIcon />
+        </Button>
       </div>
     </nav>
   );
