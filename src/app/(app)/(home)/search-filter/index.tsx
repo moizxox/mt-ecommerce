0611a8aee159241f95a395bb7a8d49f6d60d32d8
moizxox <mianmoiz899@gmail.com>
2025-09-14 04:04:02 +0500

    search bar added

--- src/app/(app)/(home)/search-filter/index.tsx
+++ src/app/(app)/(home)/search-filter/index.tsx
@@ -1,7 +1,14 @@
+import { SearchInput } from "./SearchInput";
+
 interface Props {
   data: any;
 }
 
 export const SearchFilter = ({ data }: Props) => {
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return (
+    <div className="flex flex-col gap-4  px-4 lg:px-12 py-8 border-b w-full">
+      <SearchInput />
+      {JSON.stringify(data, null, 2)}
+    </div>
+  );
 };
